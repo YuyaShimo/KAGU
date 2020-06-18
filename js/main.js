ee28f1e14eb52b2ece2633c8a0012fb900ef7771
mei0225 <mei300225@gmail.com>
2020-06-18 21:39:09 +0900

    ページトップへ戻るボタン

--- js/main.js
+++ js/main.js
@@ -54,4 +54,19 @@ $(function(){
       }
     );
 
+    // トップへ戻るボタン
+    let pagetop = $('#page_top');
+    
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {
+        pagetop.fadeIn();
+      } else {
+        pagetop.fadeOut();
+      }
+     });
+    pagetop.click(function () {
+      $('body, html').animate({ scrollTop: 0 }, 500);
+      return false;
+    });
+
 });
