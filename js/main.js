2b7ed38aed26bc82e21b767603eaa523adf03a32
mei0225 <mei300225@gmail.com>
2020-06-18 21:40:00 +0900

    ボタンを非表示

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ $(function(){
 
     // トップへ戻るボタン
     let pagetop = $('#page_top');
-    
+    pagetop.hide();
     $(window).scroll(function () {
       if ($(this).scrollTop() > 100) {
         pagetop.fadeIn();
