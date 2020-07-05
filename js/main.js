2e3b550145b818948343f525a5468416b197fc90
mei0225 <mei300225@gmail.com>
2020-07-05 22:52:22 +0900

    取り扱い家具詳細ページ>テーマの画像マウスホバー時のズームアニメーションを付与

--- js/main.js
+++ js/main.js
@@ -100,13 +100,13 @@ $(function(){
     let move = -20;
     let zoom = 1.3;
   
-    $('.item').hover(function() {       
-      width = $('.item').width() * zoom;
-      height = $('.item').height() * zoom;
+    $('.theme-img').hover(function() {       
+      width = $('.theme-img').width() * zoom;
+      height = $('.theme-img').height() * zoom;
           
       $(this).find('img').stop(false,true).animate({'width':width, 'height':height, 'top':move, 'left':move}, {duration:200});
     },
     function() {
-      $(this).find('img').stop(false,true).animate({'width':$('.item').width(), 'height':$('.item').height(), 'top':'0', 'left':'0'}, {duration:100});  
+      $(this).find('img').stop(false,true).animate({'width':$('.theme-img').width(), 'height':$('.theme-img').height(), 'top':'0', 'left':'0'}, {duration:100});  
   });
 });
